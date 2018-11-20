7ee7dca0349d4c991edc6559b43273711ad9a76a
Gustav Svendsen <gsvendsen99@gmail.com>
2018-11-20 10:43:04 +0100

    Added script for creating memory card divs with unique dataset in pairs

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const memoryBoard = document.querySelector('.memory-board');
+
+var cardCount = 15;
+
+let cardTypes = {
+  'heart': 0,
+  'skull': 0,
+  'test': 0,
+  'dog': 0,
+  'cat': 0,
+  'dollar': 0,
+  'horse': 0,
+  'apple': 0,
+}
+
+const randomProperty =  (obj)=> {
+  var keys = Object.keys(obj)
+  var randomKey = keys[Math.floor(Math.random() * (keys.length) ) + 0];
+  if(obj[randomKey] == 2){
+    return randomProperty(obj);
+  } else {
+    obj[randomKey] += 1;
+    return randomKey;
+  }
+};
+
+for(i=0; i <= cardCount; i++){
+
+  const newCardContainer = document.createElement('div')
+  const newCard = document.createElement('div');
+
+  newCardContainer.setAttribute('class', 'memory-card-container');
+  newCard.setAttribute('class', 'memory-card hidden');
+
+  newCardContainer.appendChild(newCard);
+
+
+  let randomCardType = randomProperty(cardTypes);
+
+  newCard.dataset.cardType = randomCardType;
+  memoryBoard.appendChild(newCardContainer);
+}
